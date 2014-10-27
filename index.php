086d9974534a77748247f648010124c967e76e58
113256 <jackchan715@hotmail.com>
2014-10-27 20:21:16 +0000

    company new7

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
-			echo "<td>".$registrant['company']."</td>";
+			echo "<td>".$registrant['Company']."</td>";
             echo "<td>".$registrant['date']."</td></tr>";
         }
         echo "</table>";
